e64e6381fa9515e2bc2317c29e08d00f631bcae5
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-12-02 22:27:19 +0400

    Added unique index for URL field

--- src/ShortUrl.Data/Models/ShortUrl.cs
+++ src/ShortUrl.Data/Models/ShortUrl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace ShortUrl.Data.Models
     {
         [Key]
         public String Key { get; set; }
+
         [Required]
+        [Index(IsUnique = true)]
         public String Url { get; set; }
 
         public DateTime DateCreated { get; set; }
